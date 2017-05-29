fb0b87bb7f83015591ef7cd58ee5ca42baea3064
esemin83 <esemin83@gmail.com>
2017-05-29 22:28:51 +0300

    Firefox Nightly_fullscreen+Firefox_53

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-#from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.webdriver import WebDriver
 #from selenium.webdriver.chrome.webdriver import WebDriver
 #from selenium.webdriver.ie.webdriver import WebDriver
 #from selenium.webdriver.safari.webdriver import WebDriver
@@ -32,12 +32,11 @@ class Application:
         #self.wd = webdriver.Firefox(firefox_binary="C:\\Program Files (x86)\\Nightly\\firefox.exe")
         #print(self.wd.capabilities)
 
-        browser_path = "C:\\Program Files (x86)\\Nightly\\firefox.exe"
+        #browser_path = "C:\\Program Files (x86)\\Nightly\\firefox.exe"
+        browser_path = "C:\\Program Files\\Firefox_53\\firefox.exe"
         self.wd = webdriver.Firefox(firefox_binary=browser_path)
-        self.wd.maximize_window()
-        print(self.wd.capabilities)
-
-        #webdriver.Firefox(firefox_options=)
+        #self.wd.maximize_window()
+        #print(self.wd.capabilities)
 
     def destroy(self):
         self.wd.quit()
